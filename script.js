4637f3972984a9757f718a8d57944663153d096d
SYED ALEEM HUSSAIN <49521238+sahzaleem123@users.noreply.github.com>
2019-07-11 16:03:53 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,108 @@
+//                           --------------Whatsapp Database-----------
+let whatsApp = [
+    {
+        "name": "Fabeeha Ahmed",
+        "number": "03321743825",
+        "profilePic": "https://fyf.tac-cdn.net/images/products/large/BF89-11K.jpg?auto=webp&quality=60",
+        "firstLine": "Karachi is a very big city ...",
+        "numbUnread": 1,
+        "isUnread": true,
+        "time": "12:31 PM today"
+    },
+    {
+        "name": "Arifa Choudhry",
+        "number": "+92357843644",
+        "profilePic": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRybU0g3cFQ4NnESOIPWxgnLFDQvgC1Iph3KMLmP2I_RoV3P5t6Kw",
+        "firstLine": "My name is blah blah blah",
+        "numbUnread": 2,
+        "isUnread": true,
+        "time": "09:30 AM today"
+    },
+    {
+        "name": "Farah Naz",
+        "number": "+92538457438",
+        "profilePic": "https://www.famflowerfarm.com/media/wysiwyg/_DSC0239_roze_tulpen_homepage.jpg",
+        "firstLine": "I will win!",
+        "numbUnread": 3,
+        "isUnread": true,
+        "time": "11:00 PM yesterday"
+    },
+    {
+        "name": "Alaina Faraz",
+        "number": "03324732853",
+        "profilePic": "https://pbs.twimg.com/profile_images/883859744498176000/pjEHfbdn_400x400.jpg",
+        "firstLine": "Slow and steady wins the race",
+        "numbUnread": 0,
+        "isUnread": false,
+        "time": "2:50 PM yesterday"
+    },
+    {
+        "name": "Ayesha Tariq",
+        "number": "03321432785",
+        "profilePic": "https://www.fairtrade.org.uk/~/media/FairtradeUK/Media%20Centre/Flowers.jpg?h=397&la=en&mw=760&w=760",
+        "firstLine": "I will reach at 2:30",
+        "numbUnread": 0,
+        "isUnread": false,
+        "time": "5:50 AM yesterday"
+    },
+    {
+        "name": "Taqdees Fatima",
+        "number": "03254368343",
+        "profilePic": "https://s7e5a.scene7.com/is/image/waitrose/FloristGiftsProductPod/563600_a_british-sunflowers-563600",
+        "firstLine": "I got a new job! 🎉",
+        "numbUnread": 0,
+        "isUnread": false,
+        "time": "5:30 AM yesterday"
+    },
+    {
+        "name": "Tooba Jabeen",
+        "number": "042364724722",
+        "profilePic": "https://s7img.ftdi.com/is/image/ProvideCommerce/PF_19_A3024_LAY_SHP_V2?$proflowers-tile-wide-mv-new$",
+        "firstLine": "Happy happy!",
+        "numbUnread": 13,
+        "isUnread": true,
+        "time": "3:00 PM Monday"
+    },
+    {
+        "name": "Maria Khursheed",
+        "number": "0232746325",
+        "profilePic": "https://assets.eflorist.com/site/EF-9746/Tile/Daisy.jpeg",
+        "firstLine": "Thanks!",
+        "numbUnread": 0,
+        "isUnread": false,
+        "time": "5:30 AM MOnday"
+    }
+];
+//                           --------------Tech Karo Database-----------
+let techKaro = {
+    courseName: "Tech Karo",
+    teachers: [
+        {
+            "name": "Sarah",
+            "age": "24",
+            "gender": "Female",
+            "religion": "Muslim",
+            "nationality":"Pakistani",
+        },
+        {
+            "name": "Tooba",
+            "age": "23",
+            "gender": "Female",
+            "religion": "Muslim",
+            "nationality":"Pakistani",
+        },
+        {
+            "name": "Khair-Un-Nisa",
+            "age": "22",
+            "gender": "Female",
+            "religion": "Muslim",
+            "nationality":"Pakistani",
+        }
+    ],
+    students: [
+        {
+            name:""
+        }
+    ]
+};
+
